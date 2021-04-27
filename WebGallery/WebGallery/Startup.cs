b79ce562fa27b8890c0bd79759c7f840b0c68965
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-04-27 20:26:15 +0300

    add foto

--- WebGallery/WebGallery/Startup.cs
+++ WebGallery/WebGallery/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,8 +17,20 @@ namespace WebGallery
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllers();
+
+            //services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +40,30 @@ namespace WebGallery
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.UseSwagger();
+            //app.UseSwaggerUI();
+            string fotoGirls = "foto";
+            var dir = Path.Combine(Directory.GetCurrentDirectory(), fotoGirls);
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+            app.UseStaticFiles(
+                new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(dir),//создаем FileProvider которій возвращает фотку по пути RequestPath
+                    RequestPath ="/img"
+                });
+
+            app.UseStaticFiles();
+
+
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
