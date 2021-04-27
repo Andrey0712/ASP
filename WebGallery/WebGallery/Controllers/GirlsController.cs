3516cd2d3d2fdbb6bc4978b6a9f9f34b640b1cd1
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-04-27 16:10:16 +0300

    start

---
+++ WebGallery/WebGallery/Controllers/GirlsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGallery.Controllers
+{
+    [Route("api/[controller]")]//запрос на сервер по api
+    [ApiController]
+    public class GirlsController : ControllerBase//название контролера
+    {
+        public IActionResult GetData()
+        {
+            return Ok(new { Name = "Sveta", Age = 25 });
+        }
+
+        [Route("all")]//добавляем к маршруту для нового запроса
+        public IActionResult GetData1()
+        {
+            return Ok(new { Name = "Kate", Age = 35 });
+        }
+    }
+}
